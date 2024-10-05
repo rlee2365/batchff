8ab4b671e9c93b15b3056d52c694fb41c7df2e3c
Ryan Lee <rlee130cp@gmail.com>
2024-10-05 00:52:59 -0700

    fix bitrate param

--- batchff/tasks.py
+++ batchff/tasks.py
@@ -4,5 +4,5 @@ import ffmpeg
 def process_mp3(file):
     of = replace_ext(file, 'mp3')
     stream = ffmpeg.input(file).output(
-        of, bitrate='512k')
+        of, audio_bitrate='512k')
     ffmpeg.run(stream, overwrite_output=True)
